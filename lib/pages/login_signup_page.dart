9ae75c1a09bdded71fc1003c943d59b87091e45d
Dheeraj <46556747+dheerajraina@users.noreply.github.com>
2022-03-18 15:52:48 +0530

    Initialised "invite code page" and "login signup page"

---
+++ lib/pages/login_signup_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginSignup extends StatelessWidget {
+  const LoginSignup({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Login Signup Page"),
+      ),
+    );
+  }
+}
